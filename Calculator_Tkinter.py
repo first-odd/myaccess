71bdb9d3c4ff25f46f1592b6628fbecd69fd4bc2
Jackson Andrew <2921106404@qq.com>
2021-10-28 15:43:56 +0800

    Update Calculator_Tkinter.py

--- Calculator_Tkinter.py
+++ Calculator_Tkinter.py
@@ -1,122 +1,62 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from tkinter import *
-import hashlib
-import time
 
-LOG_LINE_NUM = 0
+#设置窗口    
+x = Tk()
+x.title("Calculator")            
+x.geometry('305x460+1200+20')
+x.resizable(0,0)                
+#self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
+vary = StringVar()
+vary.set('')
+#标签
+result = Label(x, textvariable=vary,font=('',20,),width=22,height=1,anchor=W)
+result.grid(row=0, column=0)
 
-class MY_GUI():
-    def __init__(self,init_window_name):
-        self.init_window_name = init_window_name
+#功能代码部分
+def getnum(num):
+    global temp
+    temp = vary.get()
+    if temp=='0':
+        temp=''
+    temp = temp + num
+    vary.set(temp)
 
+def clear(self):
+    vary.set('')
 
-    #设置窗口
-    def set_init_window(self):
-        self.init_window_name.title("Calculator")           #窗口名
-        #self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
-        self.init_window_name.geometry('310x480+1200+20')
-        self.init_window_name.resizable(0,0)
-        #self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
-        #self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
-        #标签
-        self.init_data_label = Label(self.init_window_name, text="result:",font=('',20,))
-        self.init_data_label.grid(row=0, column=0)
-        '''self.result_data_label = Label(self.init_window_name, text="输出结果")
-        self.result_data_label.grid(row=0, column=12)'''
-        '''self.log_label = Label(self.init_window_name, text="日志")
-        self.log_label.grid(row=12, column=0)'''
-        #文本框
-        self.init_data_Text = Text(self.init_window_name, width=10, height=3,bg='lightblue')  #原始数据录入框
-        self.init_data_Text.grid(row=1, column=0)
-        '''self.result_data_Text = Text(self.init_window_name, width=7, height=4)  #处理结果展示
-        self.result_data_Text.grid(row=1, column=12, columnspan=10)'''
-        '''self.log_data_Text = Text(self.init_window_name, width=30, height=5)  # 日志框
-        self.log_data_Text.grid(row=13,column=0, columnspan=10)'''
-        #按钮
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="AC", bg="lightblue",width=8,height=6,command=self.str_trans_to_md5) 
-        self.str_trans_to_md5_button.grid(row=34, column=0, padx=1)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="一", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=34, column=1)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="X", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5) 
-        self.str_trans_to_md5_button.grid(row=34, column=2)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="/", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5) 
-        self.str_trans_to_md5_button.grid(row=34, column=3)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="1", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=35, column=0)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="2", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=35, column=1)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="3", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=35, column=2)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="4", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=35, column=3)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="5", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=36, column=0)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="6", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=36, column=1)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="7", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=36, column=2)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="8", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=36, column=3)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="9", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5) 
-        self.str_trans_to_md5_button.grid(row=37, column=0)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="0", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=37, column=1)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="+", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=37, column=2)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="=", bg="lightblue", width=8,height=6,command=self.str_trans_to_md5)  
-        self.str_trans_to_md5_button.grid(row=37, column=3)
+def run(self):
+    temp = vary.get()
+    temp.replace('X','*')
+    temp.replace('/','/')
+    answer = calculator(temp)
+    if temp == '520':
+        vary.set("我爱你")
+    else: vary.set(str(answer))
 
+def calculator(fuck):
+    s = fuck.split('+')
+    fuck = int(s[0]) + int(s[1])
+    return fuck
 
-    #功能函数
-    def str_trans_to_md5(self):
-        src = self.init_data_Text.get(1.0,END).strip().replace("\n","").encode()
-        #print("src =",src)
-        if src:
-            try:
-                myMd5 = hashlib.md5()
-                myMd5.update(src)
-                myMd5_Digest = myMd5.hexdigest()
-                #print(myMd5_Digest)
-                #输出到界面
-                self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,myMd5_Digest)
-                self.write_log_to_Text("INFO:str_trans_to_md5 success")
-            except:
-                self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,"字符串转MD5失败")
-        else:
-            self.write_log_to_Text("ERROR:str_trans_to_md5 failed")
+#第一行
+ACbutton = Button(x, text="AC", bg="lightblue",width=8,height=5,command=lambda:clear('AC')).place(x=0,y=150)
+button_ = Button(x, text="一", bg="lightblue", width=8,height=5,command=lambda:getnum('-')).place(x=76,y=150)
+buttonx = Button(x, text="X", bg="lightblue", width=8,height=5,command=lambda:getnum('X')).place(x=152,y=150)
+buttond = Button(x, text="/", bg="lightblue", width=8,height=5,command=lambda:getnum('/')).place(x=228,y=150)
+#第二行
+button1 = Button(x, text="1", bg="lightblue", width=8,height=5,command=lambda:getnum('1')).place(x=0,y=225)
+button2 = Button(x, text="2", bg="lightblue", width=8,height=5,command=lambda:getnum('2')).place(x=76,y=225)
+button3 = Button(x, text="3", bg="lightblue", width=8,height=5,command=lambda:getnum('3')).place(x=152,y=225)
+button4 = Button(x, text="4", bg="lightblue", width=8,height=5,command=lambda:getnum('4')).place(x=228,y=225)
+#第三行
+button5 = Button(x, text="5", bg="lightblue", width=8,height=5,command=lambda:getnum('5')).place(x=0,y=300)
+button6 = Button(x, text="6", bg="lightblue", width=8,height=5,command=lambda:getnum('6')).place(x=76,y=300)
+button7 = Button(x, text="7", bg="lightblue", width=8,height=5,command=lambda:getnum('7')).place(x=152,y=300)
+button8 = Button(x, text="8", bg="lightblue", width=8,height=5,command=lambda:getnum('8')).place(x=228,y=300)
+#第四行
+button9 = Button(x, text="9", bg="lightblue", width=8,height=5,command=lambda:getnum('9')).place(x=0,y=375)
+button0 = Button(x, text="0", bg="lightblue", width=8,height=5,command=lambda:getnum('0')).place(x=76,y=375)
+buttonp = Button(x, text="+", bg="lightblue", width=8,height=5,command=lambda:getnum('+')).place(x=152,y=375)
+buttone = Button(x, text="=", bg="lightblue", width=8,height=5,command=lambda:run('=')).place(x=228,y=375)
 
-
-    #获取当前时间
-    def get_current_time(self):
-        current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-        return current_time
-
-
-    #日志动态打印
-    '''def write_log_to_Text(self,logmsg):
-        global LOG_LINE_NUM
-        current_time = self.get_current_time()
-        logmsg_in = str(current_time) +" " + str(logmsg) + "\n"      #换行
-        if LOG_LINE_NUM <= 7:
-            self.log_data_Text.insert(END, logmsg_in)
-            LOG_LINE_NUM = LOG_LINE_NUM + 1
-        else:
-            self.log_data_Text.delete(1.0,2.0)
-            self.log_data_Text.insert(END, logmsg_in)'''
-
-
-def gui_start():
-    init_window = Tk()              #实例化出一个父窗口
-    ZMJ_PORTAL = MY_GUI(init_window)
-    # 设置根窗口默认属性
-    ZMJ_PORTAL.set_init_window()
-
-    init_window.mainloop()          #父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
-
-
-gui_start()
+x.mainloop()
