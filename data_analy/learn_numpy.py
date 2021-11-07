de2d6bf430af15f0073e8ca1f64b69462ce720c1
Jackson Andrew <2921106404@qq.com>
2021-11-07 12:04:08 +0800

    Update learn_pandas.py

---
+++ data_analy/learn_numpy.py
@@ -0,0 +1,323 @@
+import numpy as np    #练习使用numpy库
+import time
+import csv
+
+
+def compare():
+    t1 = time.time()
+    a = []
+    for x in range(1000000):   #[0,10) = [0,9]
+        a.append(x**2)    #在末尾添加元素
+    t2 = time.time()
+    print(t2-t1)
+
+    t3 = time.time()
+    b = np.arange(1000000)**2  # np.arange和range类似用法
+    t4 = time.time()
+    print(t4-t3)          # 可以看出 使用numpy库，比纯python代码快很多'''
+
+#数组
+def arry():
+    A1 = np.array([0,1,2,3,4,5,6,7,8,9,10],dtype='f2')     
+    '''
+    array数组,规定类型为i1:int8，可以是i2,i4,i8. 
+    b:bool 
+    u1,u2,u4,u8:无符号整数
+    f2,f4,f8:float类型
+    O:python对象
+    S:string
+    U:unicode类型
+    VA1 = A1.astype('U') 
+    用astype函数改变A1的数据格式
+    VA1 = A1[1:9:2]  #从A1[1]到A1[9],步长是2
+    切片，用冒号进行,多维数组同样操作
+    索引：
+    a[[2,3]]二维数组第3、4行的两行元素
+    a[2,3]二维数组第3行第4列的一个元素
+    a[[1,2],[4,5]]二维数组第2行第5列、第3行第6列的两个元素
+    a[1:3,4:6]二维数组第2到第4行(不包括4)、第5到第7(不包括7)的所有元素  （4个元素）
+    a[:,1]二维数组第2列全部元素 a[:,1:4]第2到第4列 a[:,[1,4]]第2列和第5列
+    bool索引：
+    直接 a<10 即可 eg：a=np.arange(24).reshape(4,6) print(a<5|a>10)
+    '''
+    #np.array创建数组
+    A2 = np.arange(10)           #arange数组，取[0,9]
+    VA2 = A2.reshape(2,5)
+    print((VA2<3)|(VA2>7))       #bool索引
+    A3 = np.arange(0,10,2)       #arange数组，每隔2个取[0,9]
+    #np.arange创建数组
+    A4 = np.random.random(2)     #随机取 0到1的数字两个组成数组
+    A5 = np.random.random((2,2)) #随机取 0到1的数字，组成2行2列的数组，多维数组
+    A6 = np.random.randint(0,10,size=(2,3)) #随机取0到10的整型数字，组成2行3列的多维数组
+    #np.random创建数组
+    A7 = np.zeros((2,2))         #都是0的2行2列多维数组
+    A8 = np.ones((2,2))          #都是1的2行2列多维数组
+    A9 = np.eye(2)               #2维单位矩阵
+    A10 = np.full((2,3),9)       #full都是9的 2行3列
+    #特殊函数创建数组
+    '''print(A1,A2,A3,A4,'\n',A5,'\n',A6,'\n',A7,'\n',A8,'\n',A9,'\n',A10)
+    print(A1.dtype) #
+    dtype获取数据类型 用astype设定类型
+    ndim获取维度
+    shape获取形状（几行几列）
+    size获取元素个数
+    itemsize获取数组中，单个元素所占字节大小
+    reshape函数可以改变np数组的形状，保持数字数量不变 eg：A1.reshape((2,2))  
+    flatten函数可以直接吧np数组变为一维数组'''
+
+def array_cal():
+    '''
+    数组和数字可以直接运算
+    数组的shape相同，可以直接运算
+    shape不同的数组运算，需要满足 广播原则:两个shape.维度从后往前数，相同或有一个是1，称为广播相融，
+    shape。size=(3,8,2)和size=(8,3)不能运算，2和3不想等，且都不是1
+    size=(3,8,2)可以和size=(8,1)运算,但是不能和size=(3,1)运算
+    但是size=(3,8,1)可以和size=(1,8)运算,得到8行8列
+    总结：行和列一定都要够，如果不够，只能为1
+    '''
+    a = np.arange(10)
+    a = a*2 #所有元素*2
+    print(a)
+    b = np.arange(20)
+    b.resize(4,5)           
+    '''resize可以改变数组 b 的形状，b.resize(4,5) 相当于 b=np.arange(20).shape(4,5) 也相当于 b=b.reshape(4,5)'''
+    c = np.random.randint(1,5,size=(4,5))
+    d = np.eye(4,5)
+    e = b+c*d
+    f = np.random.randint(1,5,size=(3,8,8))
+    g = np.random.randint(2,6,size=(8,1))
+    print(c,'\n',"="*30,'\n',d,'\n',"="*30,'\n',e,'\n',"="*30,'\n',f+g,'\n',"="*30,'\n',b)
+    '''flatten 和 ravel  都可以把数组转为一维，都不会改变原数组的形状，ravel可以直接修改原数组本身数据，flatten是副本，不能修改'''
+
+#数组的叠加、切割
+def vstack_array():
+    '''
+    行相等才能垂直方向拼接，列相等才能水平方向拼接
+    '''
+    c = np.random.randint(1,5,size=(4,5))
+    d = np.random.randint(1,6,size=(2,5))
+    e = np.vstack([c,d])    #vstack 在上下方向拼接，要求列相等   
+    d = np.vsplit(e,2)      #vsplit 横切  2:平均分成两份，要保证能均分，不然报错
+    f = np.vsplit(e,(1,2))  # e目前共有6行，从1行前、2行前切一刀,剩下4行不切
+    print(e)
+    print('='*40)
+    print(f)
+    print('='*40)
+    print(d[0])
+    print('='*40)
+    print(d[1])
+    print('='*40)
+    c = np.random.randint(1,5,size=(4,5))
+    d = np.random.randint(1,5,size=(4,3))
+    e = np.hstack([c,d])    #hstack 在左右方向拼接，要求行相等   #hsplit 竖切
+    f = np.hsplit(e,2)      #竖着平均成2份 ，不能平均会报错
+    f = np.hsplit(e,(1,3))  #分别从1列、3列前切,剩余保留
+    print(e)
+    print('='*40)
+    print(f)
+    c = np.random.randint(1,5,size=(4,5))
+    d = np.random.randint(1,5,size=(2,3))
+    e = np.concatenate([c,d],axis=None) #axis=None，都变成一维数组再拼接        
+    print(e)
+    c = np.random.randint(1,5,size=(4,5))
+    d = np.random.randint(1,5,size=(4,3))
+    e = np.concatenate([c,d],axis=1)  #axis=1,左右拼接，axis=0，上下拼接
+    f = np.split(e,2,axis=1)    #按左右平均分成2份
+    g = np.array_split(e,2,axis=1)  #array_split和split一样的用法
+    h = np.split(e,(1,2),axis=1)   #分别在1列、2列前切，剩下不切
+    print(e)
+    print('='*40)
+    print(f)
+    print('='*40)
+    print(g)
+    print('='*40)
+    print(h)
+
+    #数组的转置ndarray.T 和ndarray.transpose 一样的用法，都会修改原数组
+    t1 = np.random.randint(1,3,size=(2,3))
+    t2 = t1.T
+    print(t1)
+    print('='*40)
+    print(t2)
+
+    '''
+    深拷贝 view & 浅拷贝 copy
+    '''
+def file():
+    #CSV文件的保存和读取
+    f1 = np.random.randint(1,100,size=(30,2))
+    np.savetxt('scores.csv',f1,fmt='%d',delimiter=',',header='英语,数学',footer='',comments='')
+    #np.savetxt只能存储 1维2维数组
+    read_f1,f2 = np.loadtxt('scores.csv',dtype=int,delimiter=',',skiprows=1,unpack=True,usecols=(0,1)) 
+    # usecols=(0,1) read_f1: 第0列   f2:第1列  unpack：是否转置  loadtext默认读取成float浮点类型，需要使用dtype
+    '''print(f2)
+    print(read_f1)'''
+    '''
+    np.save(fname,array) & np.savez(fname,array) 前者存储.npy文件，后者存储.npz压缩文件,都是非文本文件
+    可以储存多维的数组
+    读取的时候用np.load(fname.npy or fname.npz)
+    '''
+    with open('scores.csv','r',encoding='utf-8') as fp:
+        reader = csv.reader(fp) #reader是一个迭代器,返回数组
+        next(reader) #跳过一行
+        for x in reader: #遍历reader 得到整个数据
+            print(x)
+            #subject = x[0]
+            #score = x[1]
+            #print({'英语':subject,'数学':score})
+    #csv.reader来读取csv文件
+
+    #csv.DictReader来读取文件 不会包含标题那一行 ，而是直接把header传入字典当作key值
+    with open('scores.csv','r') as fp1:
+        reader = csv.DictReader(fp1)  #reader直接返回一个字典
+        for x in reader:
+            #print(x)  
+            print({'英语':x['英语']})   #迭代输出标题是英语的value，此外 由于没有了标题，x[0]也不存在了，直接从x[1]开始
+            print({'数学':x['数学']})
+            #print(x[1])
+
+    #csv数据的写入 writer
+    header = ['name','age','height']
+    values = [
+        ('张三',18,180),
+        ('李四',13,180),
+        ('王五',13,180)
+    ]
+    with open('scores.csv','w',encoding='utf-8',newline='') as fp2:
+        abc = csv.writer(fp2)
+        abc.writerow(header)
+        abc.writerows(values)
+
+    #csv数据的写入  DictWriter 字典写入
+    headers = ['name','age','height']
+    values = [
+        {'name':'张三','age':18,'height':180},
+        {'name':'李四','age':14,'height':180},
+        {'name':'王五','age':15,'height':180}
+    ]
+    with open('scores.csv','w',encoding='utf-8') as fp3:
+        writer = csv.DictWriter(fp3,headers)
+        writer.writeheader()  # 需要调用 writeheader() 写一次表头
+        writer.writerows(values)
+
+'''
+处理数据缺失值
+'''
+# NAN 和 INF 的认识 & 处理 np.inf:正无穷大 -np.inf :负无穷大
+def spc_value():
+    #NAN ：not a number
+    data = np.random.randint(1,5,size=(3,4))
+    '''data[0,1] = np.NAN # 此时会报错data，因为nan是浮点类型，要保持数组内类型一致'''
+    data = data.astype(np.float64)
+    data[0,1] = np.nan
+    data[1,2] = np.nan
+    print(data)
+    '''可以用 np.isnan判断是否是nan值，不用用判断语句 np.nan == np.nan'''
+    bool = np.isnan(data) ;print(bool)# 返回判断是否为nan，返回bool数组
+    '''如果直接删除数组中的nan值，会直接输出一维数组'''
+    judge = data[np.isnan(data)] # bool索引为True
+    num= data[~np.isnan(data)]   # ~ :取反运算符
+    print(num)  # 此时输出为一位数组
+
+    #要删除nan所在行或者列 ：1.where 获取nan所在行/列   2. delete 删除这些行/列
+    data = np.random.randint(1,5,size=(5,5)).astype(np.float32)
+    data[[0,2],[1,3]] = np.nan
+    print(data)
+    lines = np.where(np.isnan(data))  
+    # where(if,1,0) where返回满足条件if的设置为1,不满足的为0,不设置1,0就返回array行列数
+    lines1 = np.where(np.isnan(data))[0] #二维数组，第一个是其行数，取其行数
+    print(lines1)
+    data1 = np.delete(data,lines1,axis=0) #按照第0层顺序删除所在行第lines1元素
+    print(data1)
+
+    '''
+    因为没有合适数据库，创建：
+    f1 = np.random.randint(10,90,size=(10,10))
+    np.savetxt('test.csv',f1,fmt='%d',delimiter=',',comments='')
+    人为去掉了其中的值'''
+    #空数据无法用loadtext解析，先用str类型解析，根据需求值替换空值，再转换成浮点类型处理数据
+    data = np.loadtxt('test.csv',encoding='utf-8',delimiter=',',dtype=str,skiprows=1,unpack=False)
+    data[data==""] = np.nan #nan替代空值
+    data1 = data.astype(np.float32)
+    data1[np.isnan(data1)] = 0  #用0替换nan
+    print(data1)
+    data2 = data1.sum(axis=1) #对第1层求和
+    print('='*80)
+    mean_data = data.astype(np.float32)
+    for x in range(mean_data.shape[1]):
+        col = mean_data[:,x]   #取每列的值
+        non_nan_col = col[~np.isnan(col)]
+        mean = non_nan_col.mean()  # 求每列的平均值
+        col[np.isnan(col)] = mean  # 把每列的平均值赋给每列的nan
+    print(mean_data)
+
+def learn_random():
+    np.random.seed(1)  #给随机数传入seed，如果不指定，跟随时间戳
+    print(np.random.rand())
+    np.random.randn()  #生成标准正态分布的随机值 均值为0，标准差为1
+    data = [1,2,2,3,4,4,5,1,2,3,]
+    np.random.choice(data,7)          #随机选择7个属于data数组的值
+    np.random.choice(data,size=(3,4)) #随机用data的值生成3行4列
+    np.random.choice(5,size=(3,4))    #随机用0到5的数组成3行4列
+    np.random.shuffle(data)           #随机打乱data数组
+
+def learn_axis():
+    a = [2,3] ; b = [3,4]; c = [[2,3],[9,7]]  #a,b,c都是列表
+    print(a+b)  # 直接相加，意思是b在a后面进行拼接 其余运算符报错，列表不能运算 
+    data = np.sum(c) #没有指定axis,会把所有元素相加返回一个num 
+    data = np.sum(c,0) #axis=0:按照最外层的顺序求和 方便理解代码,以后写成axis=0
+    data = np.sum(c,axis=1) #axis=1:按照第1层的顺序求和
+    print(data)
+    max_data = np.max(c,axis=0) 
+    print(max_data)
+
+def several_functions():
+    a = np.random.uniform(-19,20,size=(1,5))
+    print(a)
+    a_b = np.abs(a) #绝对值
+    a_c = np.sqrt(a_b)#开方
+    print(a_c)
+    '''
+    np.exp 以e为底的指数
+    np.log \np.log10 \np.log2 求对数
+    np.sign 大于0为1，小于0为-1，等于0 0
+    np.ceil 向大取整
+    np.floor 向小取整
+    np.rint / np.round 四舍五入取值
+    np.modf 分离整数和小数部分
+    np.sum 求和
+    np.prod 求积
+    np.mean 求平均值
+    np.std 求标准差
+    np.var 求方差
+    np.min np.max np.argmin 索引(下标) np.argmax
+    np.media 求中位数
+    np.any  np.all 判断是否为True
+    np.sort(ndarray,axis=) 从小到大排序 ndarray.sort() 会直接改变原数组   np.argsort() 得到排序的下标值 
+    -np.sort(-ndarray) 大到小排序
+    np.linspace(1,20,5) 1到20平均分成5次 
+    np.unique(ndarray) 数组中有哪些值 去重
+    np.unique(ndarray,return_counts=True) 数组中有哪些值，这些值出现了几次
+    https://docs.scipy.org/doc/numpy/reference/index.html
+    '''
+#自己处理的删除最大最小值取平均数的代码： 
+def delete_min_max(x):
+    get_argmax = np.argmax(x) 
+    get_argmin = np.argmin(x)
+    y1 = np.delete(x,get_argmax)
+    y2 = np.delete(y1,get_argmin)
+    y = y2.mean()
+    return y
+def learn_apply_along_axis():
+    a = np.random.randint(-6,10,size=(3,6))
+    result = np.apply_along_axis(delete_min_max,axis=1,arr=a)
+    print(result)
+
+#删除最大最小值取平均数代码case：
+def compare():
+    a = np.random.randint(-6,10,size=(3,6))
+    print(a)
+    result = np.apply_along_axis(lambda x:x[np.logical_and(x!=x.max(),x!=x.min())].mean(),axis=1,arr=a)
+    print(result)
+    
+'''ending'''
--- data_analy/learn_pandas.py
+++ data_analy/learn_pandas.py
@@ -1,9 +1,9 @@
-from numpy.core.fromnumeric import size, var
-from numpy.lib.function_base import append
-from numpy.random.mtrand import randint
+
+from numpy.core.fromnumeric import size
+from numpy.lib.shape_base import apply_along_axis, column_stack
 import pandas as pd
 import numpy as np
-from pandas.core import series
+from pandas.io.pytables import dropna_doc
 
 def kill_series():
     s = np.arange(1,6)
@@ -22,56 +22,57 @@ def kill_series():
 
     #name属性
     s = np.random.randint(1,5,size=(6,))
-    s = pd.Series(s,index=['a','b','c','d','e','f'])
+    s = pd.Series(s,index=['a','b','c','d','e','f1'])
     s.name = 'temp' #对象名
     s.index.name = 'year' #对象索引名
     print(s.head())  #s.head() 默认输出前5行   s.tail()默认输出后5行
 
-def kill_dataframe():
-    data = {'a':[1,2,3],'b':(4,5,6),'c':np.arange(7,10)} #列表、元组、数组 构成的字典
-    print(data)
-    frame = pd.DataFrame(data) # 转换成daraframe
+def kill_dframe():
+    d = {'a':[1,2,3],'b':(4,5,6),'c':np.arange(7,10)} #列表、元组、数组 构成的字典
+    print(d)
+    frame = pd.dFrame(d) # 转换成daraframe
     print(frame)  
     print('='*50)
     print(frame.values) 
     print(frame.index) #查看行索引
     print(frame.columns) #查看列索引
     print('='*50)
-    change_index = pd.DataFrame(data,index=['A','B','C'],columns=['a','b','c','e']) #keys当作了列索引，不能更改，只能添加
+    change_index = pd.dFrame(d,index=['A','B','C'],columns=['a','b','c','e']) #keys当作了列索引，不能更改，只能添加 
+                                                                              #index第0层 columns第1层
     print(change_index)
 
-    # series构成的字典，转换成dataframe
-    data1 = pd.DataFrame({'a':pd.Series(np.arange(5)),
+    # series构成的字典，转换成dframe
+    d1 = pd.dFrame({'a':pd.Series(np.arange(5)),
                           'b':pd.Series(np.arange(5,10))})
-    print(data1)
+    print(d1)
 
-    #嵌套字典，转换成dataframe
-    data2 = {'a':{'apple':3.5,'banana':3.8},
+    #嵌套字典，转换成dframe
+    d2 = {'a':{'apple':3.5,'banana':3.8},
             'b':{'apple':3,'banana':5},
             'c':{'apple':2}}
-    change_data2 = pd.DataFrame(data2)
-    print(change_data2)
+    change_d2 = pd.dFrame(d2)
+    print(change_d2)
 
-    # 二维数组 转换成dataframe
+    # 二维数组 转换成dframe
     double_list = np.arange(15).reshape(3,5)
-    vary = pd.DataFrame(double_list)
+    vary = pd.dFrame(double_list)
     print(vary)
 
-    #字典构成的列表，转换成dataframe
+    #字典构成的列表，转换成dframe
     dict_list = [{'apple':3.5,'banana':3.8},
                 {'apple':3,'banana':5},
                 {'apple':2}]
-    vary = pd.DataFrame(dict_list)
+    vary = pd.dFrame(dict_list)
     print(vary)
 
-    #series构成的列表，转换成dataframe3
+    #series构成的列表，转换成dframe3
     series_list = [pd.Series(np.random.rand(3)),pd.Series(np.random.rand(2))]
     print(series_list)
     print('='*50)
-    vary = pd.DataFrame(series_list)
+    vary = pd.dFrame(series_list)
     print(vary)
 
-    set_frame = pd.DataFrame(np.arange(9).reshape(3,3),index=['a','c','b'],columns=['A','B','C'])
+    set_frame = pd.dFrame(np.arange(9).reshape(3,3),index=['a','c','b'],columns=['A','B','C'])
     set_frame['D'] = [1,2,3] #增加D列
     print(set_frame.T) #转置
     print('='*50)
@@ -80,66 +81,125 @@ def kill_dataframe():
     '''
     index索引不可更改，需要创建新索引：reindex来新增或者改变顺序
     '''
-    f = np.arange(5)
-    data_f = pd.DataFrame(f,index=['%s'%i for i in'abcde'])
-    print(data_f)
+    f1 = np.arange(5)
+    d_f = pd.dFrame(f1,index=['%s'%i for i in'abcde'])
+    print(d_f)
     print('='*50)
-    series_f = pd.Series(f,index=['%s'%i for i in'abcde'])
+    series_f = pd.Series(f1,index=['%s'%i for i in'abcde'])
     print(series_f)
     print('='*50)
     
     #插入数据
-    data_f['g'] = 9 #添加dataframe列，标签g，所有value是9 
-    data_f.loc['f'] = [9,6] #想要添加dataframe行索引，需要loc 
-    data_f.insert(1,'h',80) #在第1列之前添加索引‘h’，value是80 这些都会直接改变data_f 
+    d_f['g'] = 9 #添加dframe列，标签g，所有value是9 
+    d_f.loc['f1'] = [9,6] #想要添加dframe行索引，需要loc 
+    d_f.insert(1,'h',80) #在第1列之前添加索引‘h’，value是80 这些都会直接改变d_f 
     series_f['g'] = 9 #添加行标签index  会直接改变series_f 
 
     f2 = pd.Series({'h':100})
-    varied_series_f = series_f.append(f2)  #新建一个series 使用append添加到series_f，不改变series_f数据
-    print(varied_series_f)  
+    d_series_f = series_f.append(f2)  #新建一个series 使用append添加到series_f，不改变series_f数据
+    print(d_series_f)  
 
-    addindex_f = data_f.reindex(['a','c','b','d','f']) #不会改变data_f
+    addindex_f = d_f.reindex(['a','c','b','d','f1']) #不会改变d_f
     row = {0:3,'h':4,'g':4}
-    varied_data_f = data_f.append(row,ignore_index=True) #新建一个字典row添加到data_f中,不会影响data_f数据,是否忽略其index，恢复默认index
-    print(varied_data_f)
+    d_d_f = d_f.append(row,ignore_index=True) #新建一个字典row添加到d_f中,不会影响d_f数据,是否忽略其index，恢复默认index
+    print(d_d_f)
 
     #删除数据  用del drop删除列，del直接删除 drop传递删除  drop(,,axis,inplace=T/F) inplace是否对原对象删除
-    data_l = pd.DataFrame({'a':np.arange(1,5),'b':3,'c':4,'d':7},index=['%s'%i for i in 'ABCD'])
-    print(data_l)
-    del data_l['a']
-    print(data_l)
-    varied_data_l = data_l.drop('b',axis=1) #默认删除行，指定axis=1删除第二层
-    varied_data_l = data_l.drop(['c','d'],axis=1) #可以删除多列
-    print(varied_data_l)
-    print(data_l)
+    d_l = pd.dFrame({'a':np.arange(1,5),'b':3,'c':4,'d':7},index=['%s'%i for i in 'ABCD'])
+    print(d_l)
+    del d_l['a']
+    print(d_l)
+    d_d_l = d_l.drop('b',axis=1) #默认删除行，指定axis=1删除第二层
+    d_d_l = d_l.drop(['c','d'],axis=1) #可以删除多列
+    print(d_d_l)
+    print(d_l)
 
     #修改数据
-    #dataframe需要用到loc标签索引
-    f = np.arange(1,16)
-    data_f = pd.DataFrame(f.reshape(5,3),index=['%s'%i for i in'abcde'],columns=['A','B','C'])
-    series_f = pd.Series(f,index=['%s'%i for i in 'abcdefghijklmno'])
+    #dframe需要用到loc标签索引
+    f1 = np.arange(1,16)
+    d_f = pd.dFrame(f1.reshape(5,3),index=['%s'%i for i in'abcde'],columns=['A','B','C'])
+    series_f = pd.Series(f1,index=['%s'%i for i in 'abcdefghijklmno'])
     series_f[0] = 99 #series只有一列，直接更改 位置索引
     series_f['b'] = 99 #直接索引更改
-    data_f['A'] = 999 #会修改A整列数据[,,,,]  标签索引
-    data_f.A = 888 #和上一步一样的效果    
-    data_f['a'] = 999 #列索引没有a，虽然行索引有，但是没用，会直接添加a列，值全是999
-    data_f.loc['a'] = 777 #a行全部更改为777
-    data_f.loc['a','A'] = 666 #对a行A列修改
+    d_f['A'] = 999 #会修改A整列数据[,,,,]  标签索引
+    d_f.A = 888 #和上一步一样的效果    
+    d_f['a'] = 999 #列索引没有a，虽然行索引有，但是没用，会直接添加a列，值全是999
+    d_f.loc['a'] = 777 #a行全部更改为777
+    d_f.loc['a','A'] = 666 #对a行A列修改
     print(series_f)
-    print(data_f)
+    print(d_f)
 
-    #查看数据 切片索引 bool索引 series_f[0] series_f['a'] series_f[0:4]位置切片顾头不顾尾，series_f['a':'f']标签切片顾头顾尾
-    #不连续索引  series：[['a','f']] [[1,5,8,0]] dataframe 不能不连续位置索引：dataframe[[1,4,5]]是无效的 :key
-    v1 = data_f[['A','a']] #取这两列
-    v2 = data_f.loc[['a','d']] #取这两行
-    v3 = data_f['A']['a']  #先写列再写行 ： A列a行
-    v4 = data_f[:2] #切片输出前2行
-    V5 = data_f.loc['a':'d','A':'C'] #先切片行标签，后切片列标签
+    #查看数据 切片索引 bool索引 series_f[0] series_f['a'] series_f[0:4]位置切片顾头不顾尾，series_f['a':'f1']标签切片顾头顾尾
+    #不连续索引  series：[['a','f1']] [[1,5,8,0]] dframe 不能不连续位置索引：dframe[[1,4,5]]是无效的 :key
+    v1 = d_f[['A','a']] #取这两列
+    v2 = d_f.loc[['a','d']] #取这两行
+    v3 = d_f['A']['a']  #先写列再写行 ： A列a行
+    v4 = d_f[:2] #切片输出前2行
+    V5 = d_f.loc['a':'d','A':'C'] #先切片行标签，后切片列标签
 
     #loc 标签索引 iloc 位置索引
-    v1 = data_f.iloc[0:2,1:3]
+    v1 = d_f.iloc[0:2,1:3]
     print(v1)
 
 def cal_pandas():
+    s1 = pd.Series(np.arange(5),index=list('ABCDE'))
+    s2 = pd.Series(np.arange(6),index=list('ABCDEF')) 
+    print(s1+s2)  # pandas的对齐运算，一方没有标签的值补充NAN
+    result = s2.add(s1,fill_value=0) #让一方不存在的标签值=0，双方都没有的值才为nan
+    print(result)
+    arr1 = np.arange(1,13).reshape(3,4)
+    print(arr1)
+    print(arr1+arr1[0])  # numpy广播性质
 
-cal_pandas()
+    d_a = pd.DataFrame(arr1,index=list('ABC'),columns=list('abcd'))
+    print(d_a)
+    print(d_a.iloc[0])
+    print(d_a.add(d_a.iloc[0])) #pandas广播性质,默认列方向：所有行加上d_a.iloc[0]
+    print(d_a.add(d_a.iloc[0],axis=1)) #等同于上一步 或者 axis='columns'
+
+def apply_pandas():
+    df = pd.DataFrame(np.random.randn(4,5),index=list('ABCD'),columns=list('abcde'))
+    ab = np.abs(df)
+    f1 = lambda x:x.max(axis=0)
+    vf1 = df.apply(f1)
+    f2 = lambda x:'%.2f'%x
+    vf2 = df.applymap(f2)
+    print(df)
+    print(vf1)
+    print(vf2)
+
+#pandas排序
+def sort_pandas():
+    df = pd.DataFrame(np.random.randint(-5,10,size=(6,6)),index=list('FEDBCA'),columns=list('fcbade'))
+    print(df)
+    #排序标签 sort_index
+    vf = df.sort_index(axis=1) #排序columns，默认升序:ascending=True
+    vf = df.sort_index(ascending=False,axis=0) #降序
+
+    #排序values
+    vf = df.sort_values(by='a') # a列 升序
+    print('*'*80)
+    print(vf)
+    jf = df.value_counts('c') #计数
+    bf = df.isin([2,5]) #是否是2或5
+    print(jf)
+    print(bf)
+ 
+
+'''
+处理缺失值
+#判断是否存在缺失值
+#丢弃缺失值 dropna()
+'''
+def nan_pandas():
+    df = pd.DataFrame([np.random.randn(3),[1,2,np.nan],[1,2,3],[np.nan,2,np.nan]])
+    print(df)
+    vf1 = df.isnull() #判断是否存在缺失值
+    print(vf1)
+    vf2 = df.dropna() #默认丢弃掉存在nan的行
+    print(vf2)
+    vf2 = df.dropna(axis=1)
+    #填充缺失值
+    vf3 = df.fillna(999)
+    print(vf3)
+nan_pandas()
