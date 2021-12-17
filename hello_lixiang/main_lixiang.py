0b7627a1928a23d68e23e2313115a52cfee8322d
Jackson Andrew <2921106404@qq.com>
2021-12-17 17:25:03 +0800

    Update main_lixiang.py

--- hello_lixiang/main_lixiang.py
+++ hello_lixiang/main_lixiang.py
@@ -1,19 +1,32 @@
 # hello lixiang 
 import re
 import csv
-
+import numpy as np
 x = str(input())
-
 def data():
     res = list(filter(None,str.split(x)))
-    print(res)
-    select_col = re.findall('第(\d+?)行',res[1])
-    select_row = re.findall('第(\d+?)列',res[1])
+    select_row = re.findall('第(\d+?)行',res[1])
+    select_col = re.findall('第(\d+?)列',res[1])
+    l = int(select_col[0])
+    h = int(select_row[0])
+    real_l = l-1
+    real_h = h-1
     get_address = re.findall("'(.*?)'",x)[0]
-    print(get_address)
-    with open (get_address,'r') as fp :
+    with open (get_address,'r',encoding='utf-8') as fp :
         reader = csv.reader(fp)
-        for a in reader :
-            print(a)
+        c = list(reader)
+        c = np.array(c)
+
+    c[real_h][real_l] = ""
+    
+    header = c[0]
+    values = []
+    for i in np.arange(1,c.shape[0]) :
+        values.append(c[i])
+    with open (get_address,'w',encoding='utf-8') as fp1:
+        abc = csv.writer(fp1)
+        abc.writerow(header)
+        abc.writerows(values)
+
 if ".csv' 删除" in x :
     data()
